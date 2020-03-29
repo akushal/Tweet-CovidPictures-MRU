43e61bd00ca386112941c000d299ab3c5c7f6080
kushal <appadudevraj7@gmail.com>
2020-03-29 18:28:24 +0400

    Confinement

--- README.md
+++ README.md
@@ -3,3 +3,29 @@
 Get  a picture from Reddit about nature
 then write stats about latest information for mauritius in the picture
 Post picture to Tweeter
+
+<img src="https://gitlab.com/appadudevraj7/tweet-covidpictures-mru/images/twitterscreenshot.png" width="240" height="320">
+
+<img src="https://gitlab.com/appadudevraj7/tweet-covidpictures-mru/images/covid.png" width="240" height="320">
+
+<img src="https://gitlab.com/appadudevraj7/tweet-covidpictures-mru/images/nodered.png" width="240" height="320">
+
+# Requirements
+- Imagemagick
+- Bash
+- Nodered Installed
+- Twitter Node for Nodered
+- Twitter Access Tokens from Twitter
+
+# Flow 
+- Nodered executes the script bash to get a picture from Reddit NaturePics 
+- Gathers data from Covid Tracker API to get stats about Mauritius
+- Write the stats on the image using imagemagick command "convert"
+- Save the picture on a path
+- Load the picture as a buffer stream on Nodered
+- Add the parameter msg.media -> which will allow twitter node to post the picture
+- Get Stats about the API from covid tracker API and put it as Status Text on the tweeter post
+- Appending Any text function will get the exact text for tweeter posting
+- Finally tweet the image along the the Text
+
+ 
---
+++ imagegen.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+##################################
+ Script to Download a picture 
+ get stats about covid 
+ write it on the image
+#################################
+
+
+temppath=/var/www/html/flight/temp.png
+confinementstartdate='200320' #Mauritius
+finalimagepath=/var/www/html/flight/covid.png
+backup=/var/www/html/flight/backup.png
+
+#Backup and remove file
+if [ -f $finalimagepath ]; then
+
+	mv  $finalimagepath "$backup"
+fi
+
+
+imageURL=$(curl -s 'https://apiimages.kushal.net/api/v1.0/r/naturepics/random' | \
+	python2 -c "import sys, json; print json.load(sys.stdin)['url']")
+wget --quiet $imageURL -O $temppath >/dev/null
+
+COVIDDS=$(curl -s 'https://coronavirus-tracker-api.herokuapp.com/v2/locations?country_code=MU')
+
+confirmed=`echo $COVIDDS |jq .latest.confirmed`
+recovered=`echo $COVIDDS | jq .latest.recovered`
+deaths=`echo $COVIDDS | jq .latest.deaths`
+
+numconfinementdays=$(( ($(date --date="now" +%s) - $(date --date="$confinementstartdate" +%s) )/(60*60*24) ))
+
+#echo "$numconfinementdays"
+
+#message='"Confirmed cases: '"$confirmed"', Recovered cases: '"$recovered"', Deaths:'"$deaths"', Confinement#'"$numconfinementdays"'"'
+#echo "$message"
+confinementmsg='"#MAURITIUS Confinement#'"$numconfinementdays"'"'
+confirmedmsg='"Confirmed:'"$confirmed"'"'
+recoveredmsg='"Recovered:'"$recovered"'"'
+deathmsg='"Deaths:'"$deaths"'"'
+
+convert -font helvetica -fill white -pointsize 50 -gravity north \
+	-draw "text 0,200 $confinementmsg" \
+	-gravity center -draw "text 0,0 $confirmedmsg" \
+	-draw "text 0,50 $recoveredmsg" \
+	-draw "text 0,100 $deathmsg" \
+	-gravity south -draw "text 0,100 '#StaySafe #StayHome'" $temppath $finalimagepath 
+
+if [ $? -eq 0 ];then
+	echo "[OK]"
+	exit 0
+else
+	echo "KO"
+	exit 1
+fi
+#convert -font helvetica -fill white -pointsize 40 -gravity center -draw "  $message" $temppath $finalimagepath
+#echo "$message"  | convert -font helvetica -fill white -pointsize 60 -gravity center caption:@- $temppath 
+
